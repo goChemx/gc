987a55a2c5c5ef82a9964d3e6a3608e19817af72
goChemx <86946369+goChemx@users.noreply.github.com>
2021-07-21 01:12:18 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -322,7 +322,62 @@
 
     };
 
-
+    
+    
+   /* Contact Form ---- Formspark
+    * ------------------------------------------------------ */
+    var clContactForm = function() {
+        
+        /* local validation */
+        $('#contactForm').validate({
+        
+            /* submit via ajax */
+            submitHandler: function(form) {
+    
+                var sLoader = $('.submit-loader');
+    
+                $.ajax({
+    
+                    method: "POST",
+                    url: "https://submit-form.com/iB0A4735",
+                    dataType: "json",
+                    data: $(form).serialize(),
+                    beforeSend: function() { 
+    
+                        sLoader.slideDown("slow");
+    
+                    },
+                    success: function(msg) {
+    
+                        // Message was sent
+                        //if (msg == 'OK') {
+                            sLoader.slideUp("slow"); 
+                            $('.message-warning').fadeOut();
+                            $('#contactForm').fadeOut();
+                            $('.message-success').fadeIn();
+                        //}
+                        // There was an error
+                        //else {
+                        //    sLoader.slideUp("slow"); 
+                        //    $('.message-warning').html(msg);
+                        //    $('.message-warning').slideDown("slow");
+                        //}
+    
+                    },
+                    error: function() {
+    
+                        sLoader.slideUp("slow"); 
+                        $('.message-warning').html("Something went wrong. Please try again.");
+                        $('.message-warning').slideDown("slow");
+    
+                    }
+    
+                });
+            }
+    
+        });
+    }; 
+    
    /* Contact Form ---- Formspree
     * ------------------------------------------------------ */
     var clContactForm = function() {
